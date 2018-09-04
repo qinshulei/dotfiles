1f4ac93e3a3dc58f4eb19bd33b169e977d16a40c
qinshulei <527072230@qq.com>
2018-09-04 15:29:59 +0800

    move xdotool into lib

--- bin/xdotool-help-function.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-function swap_to(){
-    # swap to chrome
-    local name="$@"
-    local PID=$( xdotool search --limit 1 --name "${name}" )
-    xdotool windowactivate $PID
-    xdotool sleep 1
-}
--- bin/xdotool-set-size.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# xdotool search chrome
-# open a new table
-# xdotool search chrome
-# compare find the new window id
-#xdotool active 71304336
-xdotool windowsize 71304336 1024 768
