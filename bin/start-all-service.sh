939e7da5f21f3d88ebf4dde778da8f8a804d0526
qinshulei <527072230@qq.com>
2019-11-25 10:15:08 +0800

    change start all service

--- bin/start-all-service.sh
+++ bin/start-all-service.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-nohup /usr/bin/fcitx-autostart 2>&1 >/dev/null &
-nohup synergy  2>&1 >/dev/null  &
+# nohup /usr/bin/fcitx-autostart 2>&1 >/dev/null &
+# nohup synergy  2>&1 >/dev/null  &
