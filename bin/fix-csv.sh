7ad54a0c297454b0ea0a2a7c827f91875f3212b2
qinshulei <527072230@qq.com>
2019-12-31 11:28:48 +0800

    support parameters

--- bin/fix-csv.sh
+++ bin/fix-csv.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+fileName=$1
+if [ -z "${fileName}" ];then
+    echo "Error: you must input the file name"
+    exit 1
+fi
+
 echo -n -e "\\xEF\\xBB\\xBF" > csv.csv
 cat ${fileName} >> csv.csv
 rm -rf ./${fileName}
